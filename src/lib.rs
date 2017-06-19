50061106e9a5af7e46fa2b78a4e70b71c9cf662e
Andrew Hobden <andrew@hoverbear.org>
2017-06-19 22:20:31 +0200

    Println

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,27 @@
 #![feature(lang_items)]
 #![feature(compiler_builtins_lib)]
+#![feature(const_fn)]
+#![feature(unique)]
 #![no_std]
 
+extern crate volatile;
 extern crate rlibc;
 extern crate compiler_builtins;
+extern crate spin;
+
+#[macro_use] 
+mod vga_buffer;
+
+use vga_buffer::Writer;
 
 #[no_mangle]
 pub extern fn rust_main() {
-     let test = (0..3).flat_map(|x| 0..x).zip(0..);
+    // ATTENTION: we have a very small stack and no guard page
+    vga_buffer::clear_screen();
+    println!("Hello World{}", "!");
+    println!("{}", { println!("inner"); "outer" });
+
+    loop{}
 }
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
